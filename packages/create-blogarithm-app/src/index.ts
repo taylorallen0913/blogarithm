b680c423e53ed0b8153d8176bd17ff3f570816de
Taylor Allen <taylorallen0913@gmail.com>
2020-06-27 10:14:07 -0700

    template copies properly

--- packages/create-blogarithm-app/src/index.ts
+++ packages/create-blogarithm-app/src/index.ts
@@ -1,15 +1,61 @@
 #!/usr/bin/env node
 
+import path from 'path';
 import args from 'args';
+import clear from 'clear';
+import chalk from 'chalk';
+import figlet from 'figlet';
+import listr from 'listr';
 import fs from 'fs';
-import { displayHeader } from './utils/header';
+import ncp from 'ncp';
+
+const displayHeader = (): void => {
+  clear();
+  console.log(chalk.blue(figlet.textSync('Blogarithm') + '\n'));
+};
+
+const copyTemplate = (dir: string, templateDir: string): void => {
+  ncp(templateDir, dir, (err) => {
+    if (err) console.log(err);
+  });
+};
+
+const initGit = async () => {};
+
+const runTasks = async (dir: string, templateDir: string): Promise<void> => {
+  const tasks = new listr([
+    {
+      title: 'Copying template',
+      task: async () => {
+        copyTemplate(dir, templateDir);
+      },
+    },
+    {
+      title: 'Initializing GitHub repository',
+      task: () => {},
+    },
+  ]);
+  await tasks.run();
+};
 
 const createProject = (args: Array<string>): void => {
   if (args[args.indexOf('init') + 1] !== undefined) {
-    displayHeader();
     const projectName = args[args.indexOf('init') + 1];
-    fs.mkdirSync(projectName);
-    console.log(projectName);
+    fs.mkdir(projectName, async (err) => {
+      if (err) {
+        console.log(
+          `${chalk.red('ERROR')} –– ${chalk.yellow(
+            'Directory with project name already exists',
+          )}`,
+        );
+        process.exit();
+      }
+      const dir = projectName;
+      // change later to dynamic template
+      const templateDir = path.resolve(__dirname, '../templates/javascript');
+      displayHeader();
+      await runTasks(dir, templateDir);
+    });
   }
 };
 
--- packages/create-blogarithm-app/src/utils/header.ts
+++
@@ -1,6 +0,0 @@
-import figlet from 'figlet';
-import chalk from 'chalk';
-
-export const displayHeader = async () => {
-  console.log(chalk.blue(figlet.textSync('Blogarithm') + '\n'));
-};
