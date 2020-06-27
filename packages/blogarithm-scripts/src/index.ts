31782bda4ec6807f23f563f9bd3effb3b566d109
Taylor Allen <taylorallen0913@gmail.com>
2020-06-27 11:11:16 -0700

    added boilerplate for scripts and changed  names

--- packages/blogarithm-scripts/src/index.ts
+++ packages/blogarithm-scripts/src/index.ts
@@ -1,1 +1,11 @@
-export const add = (x: number, y: number): number => x + y;
+#!/usr/bin/env node
+
+import fs from 'fs';
+import args from 'args';
+import chalk from 'chalk';
+import start from './start';
+import build from './build';
+
+args
+  .command('start', 'Starts app on development server', start)
+  .command('build', 'Builds and optimizes project', build);
